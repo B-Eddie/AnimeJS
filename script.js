56a839b37be459aa6508655e34a9ae4a71d17cd6
eddieb <eddiewird@gmail.com>
2024-07-03 23:16:52 -0400

    bruh wrong id again

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const canvas = document.getElementById('Canvas');
+    const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
     canvas.width = window.innerWidth;
